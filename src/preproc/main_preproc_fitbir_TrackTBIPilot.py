51617d32bfaea3dc73634b304649c3ce21b38d6d
Anand A Joshi <ajoshi@masal.usc.edu>
2018-09-29 01:16:09 -0700

    update

---
+++ src/preproc/main_preproc_fitbir_TrackTBIPilot.py
@@ -0,0 +1,16 @@
+%||AUM||
+%||Shree Ganeshaya Namaha||
+
+import pandas as pd
+
+
+def main():
+
+    med_hist_csv = 
+
+
+subIds = pd.read_csv(cogscore_file, index_col=0)
+    
+    ''' If fMRI data exists for some subjects, then store their cognitive scores '''
+    for subid in self.subids:
+        self.cog_scores.append(self.get_cog_score_subid(subid))
