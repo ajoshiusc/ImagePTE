760f7d36d42c32d07bababda65ffa58ffbb668d1
Anand A Joshi <ajoshi@masal.usc.edu>
2018-10-01 16:56:38 -0700

    update

--- src/preproc/main_preproc_fitbir_TrackTBIPilot.py
+++ src/preproc/main_preproc_fitbir_TrackTBIPilot.py
@@ -5,7 +5,7 @@ import pandas as pd
 import glob
 import os
 import shutil
-
+from fitbirpre import zip2nii
 
 def main():
 
@@ -43,42 +43,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-import zipfile
-import tempfile
-from nipype.interfaces.dcm2nii import Dcm2niix
-
-def zip2nii(zipfname, outdir):
-
-    with tempfile.TemporaryDirectory() as tmpdir:
-        with tempfile.TemporaryDirectory() as outtmpdir:
-            shutil.copy(zipfname, tmpdir)
-            pth, fname = os.path.split(zipfname)
-            fname = os.path.join(tmpdir, fname)
-
-            zip_ref = zipfile.ZipFile(fname, 'r')
-            zip_ref.extractall(tmpdir)
-            zip_ref.close()
-            os.remove(fname)
-            
-            converter = Dcm2niix()
-            converter.inputs.source_dir = tmpdir
-            converter.inputs.compression = 5
-            converter.inputs.output_dir = outtmpdir
-            print(converter.cmdline)
-    #'dcm2niix -b y -z y -5 -x n -t n -m n -o ds005 -s n -v n tmpdir'
-            converter.run() 
-            dirlist = glob.glob(outtmpdir + '/' + '*.gz')
-            for file_name in dirlist:
-                if (os.path.isfile(file_name)):
-                    shutil.copy(file_name, outdir)
-
-
-
-
-
-
-
-
-
-
