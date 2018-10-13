0e343540f46d4bd8af986f9c3517928ac79a7f1d
Anand A Joshi <ajoshi@masal.usc.edu>
2018-10-13 15:06:35 -0700

    plot png files

--- src/preproc/main_visualize_fitbir_TrackTBIPilot.py
+++ src/preproc/main_visualize_fitbir_TrackTBIPilot.py
@@ -8,6 +8,7 @@ import shutil
 from fitbirpre import zip2nii, reg2mni, name2modality
 import nilearn.image as ni
 from nilearn import plotting
+import numpy as np
 
 def main():
     #Set subject dirs
@@ -26,25 +27,41 @@ def main():
 
         dirname = os.path.join(preproc_dir, study_name, subid)
 
-
         fnamet1 = os.path.join(dirname, 'T1.nii.gz')
         fnamet2 = os.path.join(dirname, 'T2.nii.gz')
         fnameflair = os.path.join(dirname, 'FLAIR.nii.gz')
 
-        print('+++++++++++'+subid+'++++++++++++++')
+        print('+++++++++++' + subid + '++++++++++++++')
 
         if os.path.isfile(fnamet1):
             imgt1 = ni.load_img(fnamet1)
-            plotting.plot_anat(imgt1, title="plot_anat", cut_coords=(0,0,0))
+            plotting.plot_anat(
+                imgt1,
+                title="plot_anat",
+                cut_coords=(0, 0, 0),
+                output_file=subid + '_T1.png',
+                vmin=0,
+                vmax=np.percentile(imgt1.get_data().flatten(), 95))
 
         if os.path.isfile(fnamet2):
             imgt2 = ni.load_img(fnamet2)
-            plotting.plot_anat(imgt2, title="plot_anat", cut_coords=(0,0,0))
+            plotting.plot_anat(
+                imgt2,
+                title="plot_anat",
+                cut_coords=(0, 0, 0),
+                output_file=subid + '_T2.png',
+                vmin=0,
+                vmax=np.percentile(imgt2.get_data().flatten(), 95))
 
         if os.path.isfile(fnameflair):
             imgflair = ni.load_img(fnameflair)
-            plotting.plot_anat(imgflair, title="plot_anat", cut_coords=(0,0,0))
-
+            plotting.plot_anat(
+                imgflair,
+                title="plot_anat",
+                cut_coords=(0, 0, 0),
+                output_file=subid + '_FLAIR.png',
+                vmin=0,
+                vmax=np.percentile(imgflair.get_data().flatten(), 95))
 
 
 if __name__ == "__main__":
