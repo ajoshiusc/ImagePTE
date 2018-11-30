59c55778eb5367fdb4786fd797da9490f4b4374a
Anand A Joshi <ajoshi@masal.usc.edu>
2018-11-30 01:16:12 -0800

    update

--- src/preproc/main_preproc_fitbir_Maryland_Rao_prospective_mnireg.py
+++ src/preproc/main_preproc_fitbir_Maryland_Rao_prospective_mnireg.py
@@ -40,8 +40,6 @@ def main():
     preproc_dir = '/big_disk/ajoshi/fitbir/preproc'
     subIds = pd.read_csv(med_hist_csv, index_col=1)
 
-    pool = Pool(processes=4)
-
     for subid in subIds.index:
         print(subid)
 
@@ -54,14 +52,37 @@ def main():
             subdir = os.path.join(preproc_dir, study_name, subid)
             os.makedirs(subdir)
 
-            # Normalize all images to 1mm res space.
-            imgfiles = dirlist  #glob.glob(img_subdir + '/*.nii.gz')
-            print('running proc func')
-            print('done proc func')
-            pool.starmap(regparfun, zip(repeat(subdir), imgfiles))
+            # Normalize all images to standard MNI space.
+            imgfiles = dirlist
 
-    pool.close()
-    pool.join()
+            t1 = os.path.join(study_name, 'T1.nii.gz')
+            t1mni = os.path.join(study_name, 'T1_mni.nii.gz')
+            t1mnimat = os.path.join(study_name, 'T1mni.mat')
+
+            # register T1 image to MNI space
+            os.system('../first_flirt_rigid ' + t1 + ' ' + t1mni)
+
+            t2 = os.path.join(study_name, 'T2.nii.gz')
+            t2r = os.path.join(study_name, 'T2r.nii.gz')
+            t2mni = os.path.join(study_name, 'T2mni.nii.gz')
+            if os.path.isfile(t2):
+                # Register T2 to T1
+                os.system('flirt -in ' + t2 + ' -nosearch -out ' + t2r +
+                          '-ref ' + t1 + ' -dof 6')
+                # Apply the same transform (T1->MNI) to registered T2 to take it to mni space
+                os.system('flirt -in ' + t2r + ' -ref ' + t1mni +
+                          '-applyxfm -init ' + t1mnimat + ' -out ' + t2mni)
+
+            flair = os.path.join(study_name, 'FLAIR.nii.gz')
+            flairr = os.path.join(study_name, 'FLAIRr.nii.gz')
+            flairmni = os.path.join(study_name, 'FLAIRmni.nii.gz')
+            if os.path.isfile(flair):
+                # Register FLAIR to T1
+                os.system('flirt -in ' + flair + ' -nosearch -out ' + flairr +
+                          '-ref ' + t1 + ' -dof 6')
+                # Apply the same transform (T1->MNI) to registered FLAIR to take it to mni space
+                os.system('flirt -in ' + flairr + ' -ref ' + t1mni +
+                          '-applyxfm -init ' + t1mnimat + ' -out ' + flairmni)
 
 
 if __name__ == "__main__":
