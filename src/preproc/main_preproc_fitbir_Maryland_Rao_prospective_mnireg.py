d6d7718bcbc4f2d8f87768774a7c9de5871a0a9f
Anand A Joshi <ajoshi@masal.usc.edu>
2018-11-30 12:11:05 -0800

    update to the code

--- src/preproc/main_preproc_fitbir_Maryland_Rao_prospective_mnireg.py
+++ src/preproc/main_preproc_fitbir_Maryland_Rao_prospective_mnireg.py
@@ -10,80 +10,69 @@ import numbers
 from shutil import copyfile
 
 
-def regparfun(subdir, infile):
-    modname = name2modality(infile)
-    if modname is not None:
-        outfname = os.path.join(subdir, modname)
-    else:
-        outfname = ''
+def regparfun(subid):
 
-    if (modname is not None) and (modname is not 'rest'):
-        if not os.path.isfile(outfname + '.nii.gz'):
-            os.system('flirt -in ' + infile + ' -ref ' + infile + ' -out ' +
-                      outfname + '.nii.gz' + ' -applyisoxfm 1')
+    study_dir = '/big_disk/ajoshi/fitbir/maryland_rao/MM_Prospective_ImagingMR_314'
+    # List of subjects that maryland_rao
+    tbi_done_list = '/big_disk/ajoshi/fitbir/preproc/maryland_rao_done.txt'
+    preproc_dir = '/big_disk/ajoshi/fitbir/preproc'
+    study_name = 'maryland_rao_v1'
 
+    dirlist = glob.glob(study_dir + '*/' + subid + '*_mrtbi_*v1*.nii')
+    print(dirlist)
+    if len(dirlist) > 0:
+        subdir = os.path.join(preproc_dir, study_name, subid)
 
-#            reg2mni_re(infile=infile, outfile=outfname)
+        # Normalize all images to standard MNI space.
+        imgfiles = dirlist
 
-    if modname is 'rest':
-        copyfile(infile, outfname + '.nii.gz')
+        t1 = os.path.join(subdir, 'T1.nii.gz')
+        t1mni = os.path.join(subdir, 'T1mni.nii.gz')
+        t1mnimat = os.path.join(subdir, 'T1mni.mat')
+        if not os.path.isfile(t1):
+            return
+
+        # register T1 image to MNI space
+        os.system('./first_flirt_rigid ' + t1 + ' ' + t1mni)
+
+        t2 = os.path.join(subdir, 'T2.nii.gz')
+        t2r = os.path.join(subdir, 'T2r.nii.gz')
+        t2mni = os.path.join(subdir, 'T2mni.nii.gz')
+        if os.path.isfile(t2):
+            # Register T2 to T1
+            os.system('flirt -in ' + t2 + ' -nosearch -out ' + t2r + ' -ref ' +
+                      t1 + ' -dof 6')
+            # Apply the same transform (T1->MNI) to registered T2 to take it to mni space
+            os.system('flirt -in ' + t2r + ' -ref ' + t1mni +
+                      ' -applyxfm -init ' + t1mnimat + ' -out ' + t2mni)
+
+        flair = os.path.join(subdir, 'FLAIR.nii.gz')
+        flairr = os.path.join(subdir, 'FLAIRr.nii.gz')
+        flairmni = os.path.join(subdir, 'FLAIRmni.nii.gz')
+        if os.path.isfile(flair):
+            # Register FLAIR to T1
+            os.system('flirt -in ' + flair + ' -nosearch -out ' + flairr +
+                      ' -ref ' + t1 + ' -dof 6')
+            # Apply the same transform (T1->MNI) to registered FLAIR to take it to mni space
+            os.system('flirt -in ' + flairr + ' -ref ' + t1mni +
+                      ' -applyxfm -init ' + t1mnimat + ' -out ' + flairmni)
 
 
 def main():
     #Set subject dirs
-    study_name = 'maryland_rao_v1'
     med_hist_csv = '/big_disk/ajoshi/fitbir/maryland_rao/FITBIR Demographics_314/FITBIRdemographics_prospective.csv'
-    study_dir = '/big_disk/ajoshi/fitbir/maryland_rao/MM_Prospective_ImagingMR_314'
-
-    # List of subjects that maryland_rao
-    tbi_done_list = '/big_disk/ajoshi/fitbir/preproc/maryland_rao_done.txt'
-    preproc_dir = '/big_disk/ajoshi/fitbir/preproc'
     subIds = pd.read_csv(med_hist_csv, index_col=1)
-
+   # pool = Pool(processes=12)
     for subid in subIds.index:
         print(subid)
-
         if not isinstance(subid, str):
             continue
 
-        dirlist = glob.glob(study_dir + '*/' + subid + '*_mrtbi_*v1*.nii')
-        print(dirlist)
-        if len(dirlist) > 0:
-            subdir = os.path.join(preproc_dir, study_name, subid)
+        regparfun(subid)
+        #pool.starmap(regparfun, subid)
 
-            # Normalize all images to standard MNI space.
-            imgfiles = dirlist
-
-            t1 = os.path.join(subdir, 'T1.nii.gz')
-            t1mni = os.path.join(subdir, 'T1_mni.nii.gz')
-            t1mnimat = os.path.join(subdir, 'T1mni.mat')
-            if not os.path.isfile(t1):
-                continue
-
-            # register T1 image to MNI space
-            os.system('./first_flirt_rigid ' + t1 + ' ' + t1mni)
-
-            t2 = os.path.join(subdir, 'T2.nii.gz')
-            t2r = os.path.join(subdir, 'T2r.nii.gz')
-            t2mni = os.path.join(subdir, 'T2mni.nii.gz')
-            if os.path.isfile(t2):
-                # Register T2 to T1
-                os.system('flirt -in ' + t2 + ' -nosearch -out ' + t2r +
-                          '-ref ' + t1 + ' -dof 6')
-                # Apply the same transform (T1->MNI) to registered T2 to take it to mni space
-                os.system('flirt -in ' + t2r + ' -ref ' + t1mni +
-                          ' -applyxfm -init ' + t1mnimat + ' -out ' + t2mni)
-
-            flair = os.path.join(subdir, 'FLAIR.nii.gz')
-            flairr = os.path.join(subdir, 'FLAIRr.nii.gz')
-            flairmni = os.path.join(subdir, 'FLAIRmni.nii.gz')
-            if os.path.isfile(flair):
-                # Register FLAIR to T1
-                os.system('flirt -in ' + flair + ' -nosearch -out ' + flairr +
-                          '-ref ' + t1 + ' -dof 6')
-                # Apply the same transform (T1->MNI) to registered FLAIR to take it to mni space
-                os.system('flirt -in ' + flairr + ' -ref ' + t1mni +
-                          ' -applyxfm -init ' + t1mnimat + ' -out ' + flairmni)
+    pool.close()
+    pool.join()
 
 
 if __name__ == "__main__":
