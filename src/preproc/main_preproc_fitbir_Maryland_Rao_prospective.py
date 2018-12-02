80cccc3b06848a9dc00af192e8d11b16749445bf
Anand A Joshi <ajoshi@masal.usc.edu>
2018-12-01 18:57:44 -0800

    update

--- src/preproc/main_preproc_fitbir_Maryland_Rao_prospective.py
+++ src/preproc/main_preproc_fitbir_Maryland_Rao_prospective.py
@@ -21,19 +21,6 @@ def regparfun(subdir, infile):
     copyfile(infile, outfname + '.nii')
 
 
-"""     if (modname is not None) and (modname is not 'rest'):
-        if not os.path.isfile(outfname + '.nii.gz'):
-            os.system('flirt -in ' + infile + ' -ref ' + infile + ' -out ' +
-                      outfname + '.nii.gz' + ' -applyisoxfm 1')
-
-
-#            reg2mni_re(infile=infile, outfile=outfname)
-
-    if modname is 'rest':
-        copyfile(infile, outfname + '.nii.gz')
- """
-
-
 def main():
     #Set subject dirs
     study_name = 'maryland_rao_v1'
@@ -41,7 +28,6 @@ def main():
     study_dir = '/big_disk/ajoshi/fitbir/maryland_rao/MM_Prospective_ImagingMR_314'
 
     # List of subjects that maryland_rao
-    tbi_done_list = '/big_disk/ajoshi/fitbir/preproc/maryland_rao_done.txt'
     preproc_dir = '/big_disk/ajoshi/fitbir/preproc'
     subIds = pd.read_csv(med_hist_csv, index_col=1)
 
@@ -54,15 +40,12 @@ def main():
             continue
 
         dirlist = glob.glob(study_dir + '*/' + subid + '*_mrtbi_*v1*.nii')
-        print(dirlist)
         if len(dirlist) > 0:
             subdir = os.path.join(preproc_dir, study_name, subid)
             os.makedirs(subdir)
 
             # Normalize all images to 1mm res space.
             imgfiles = dirlist  #glob.glob(img_subdir + '/*.nii.gz')
-            print('running proc func')
-            print('done proc func')
             pool.starmap(regparfun, zip(repeat(subdir), imgfiles))
 
     pool.close()
@@ -71,10 +54,6 @@ def main():
     for subid in subIds.index:
 
         if not isinstance(subid, str):
-            continue
-
-        if any(subid in s for s in tbidoneIds):
-            print(subid + ' is already done')
             continue
 
         dirname = os.path.join(preproc_dir, study_name, subid)
