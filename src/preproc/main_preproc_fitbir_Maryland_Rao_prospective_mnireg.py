5fa67071d0d37e99b1a7a23d75a4ef8a4be13ff8
Anand A Joshi <ajoshi@masal.usc.edu>
2018-12-01 22:51:19 -0800

    update

--- src/preproc/main_preproc_fitbir_Maryland_Rao_prospective_mnireg.py
+++ src/preproc/main_preproc_fitbir_Maryland_Rao_prospective_mnireg.py
@@ -19,40 +19,33 @@ def regparfun(subid):
     preproc_dir = '/big_disk/ajoshi/fitbir/preproc'
     study_name = 'maryland_rao_v1'
 
-    dirlist = glob.glob(study_dir + '*/' + subid + '*_mrtbi_*v1*.nii')
-    print(dirlist)
-    #    time.sleep(5)
+    subdir = os.path.join(preproc_dir, study_name, subid)
 
-    if len(dirlist) > 0:
-        subdir = os.path.join(preproc_dir, study_name, subid)
+    t1 = os.path.join(subdir, 'T1r.nii.gz')
+    t1mni = os.path.join(subdir, 'T1mni.nii.gz')
+    t1mnimat = os.path.join(subdir, 'T1mni.mat')
+    print(subid)
 
-        # Normalize all images to standard MNI space.
-        imgfiles = dirlist
+    if not os.path.isfile(t1):
+        return
 
-        t1 = os.path.join(subdir, 'T1r.nii.gz')
-        t1mni = os.path.join(subdir, 'T1mni.nii.gz')
-        t1mnimat = os.path.join(subdir, 'T1mni.mat')
-        print(subid)
+    # register T1 image to MNI space
+    os.system('first_flirt ' + t1 + ' ' + t1mni)
 
-        if not os.path.isfile(t1):
-            return
+    t2 = os.path.join(subdir, 'T2r.nii.gz')
+    t2mni = os.path.join(subdir, 'T2mni.nii.gz')
+    if os.path.isfile(t2):
+        # Apply the same transform (T1->MNI) to registered T2 to take it to mni space
+        os.system('flirt -in ' + t2 + ' -ref ' + t1mni +
+                  ' -dof 6 -cost normmi -init ' + t1mnimat + ' -out ' + t2mni)
 
-        # register T1 image to MNI space
-        os.system('./first_flirt ' + t1 + ' ' + t1mni)
-
-        t2 = os.path.join(subdir, 'T2r.nii.gz')
-        t2mni = os.path.join(subdir, 'T2mni.nii.gz')
-        if os.path.isfile(t2):
-            # Apply the same transform (T1->MNI) to registered T2 to take it to mni space
-            os.system('flirt -in ' + t2 + ' -ref ' + t1mni +
-                      ' -dof 6 -cost normmi -init ' + t1mnimat + ' -out ' + t2mni)
-
-        flair = os.path.join(subdir, 'FLAIRr.nii.gz')
-        flairmni = os.path.join(subdir, 'FLAIRmni.nii.gz')
-        if os.path.isfile(flair):
-            # Apply the same transform (T1->MNI) to registered FLAIR to take it to mni space
-            os.system('flirt -in ' + flair + ' -ref ' + t1mni +
-                      ' -dof 6 -cost normmi -init ' + t1mnimat + ' -out ' + flairmni)
+    flair = os.path.join(subdir, 'FLAIRr.nii.gz')
+    flairmni = os.path.join(subdir, 'FLAIRmni.nii.gz')
+    if os.path.isfile(flair):
+        # Apply the same transform (T1->MNI) to registered FLAIR to take it to mni space
+        os.system('flirt -in ' + flair + ' -ref ' + t1mni +
+                  ' -dof 6 -cost normmi -init ' + t1mnimat + ' -out ' +
+                  flairmni)
 
 
 def main():
