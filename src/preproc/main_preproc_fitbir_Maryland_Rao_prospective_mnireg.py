0e52f5036a7420ae96a4e6bd1244fcd477c62f9e
Anand A Joshi <ajoshi@masal.usc.edu>
2018-12-03 18:05:24 -0800

    update

--- src/preproc/main_preproc_fitbir_Maryland_Rao_prospective_mnireg.py
+++ src/preproc/main_preproc_fitbir_Maryland_Rao_prospective_mnireg.py
@@ -22,6 +22,7 @@ def regparfun(subid):
 
     t1 = os.path.join(subdir, 'T1r.nii.gz')
     t1mni = os.path.join(subdir, 'T1mni.nii.gz')
+    t1bse = os.path.join(subdir, 'T1.bse.nii.gz')
     t1mnimat = os.path.join(subdir, 'T1mni.mat')
     print(subid)
 
@@ -30,22 +31,30 @@ def regparfun(subid):
 
     # register T1 image to MNI space
     #os.system('./first_flirt_rigid_reorient_mi ' + t1 + ' ' + t1mni)
-    os.system('flirt -in ' + t1 + ' -out ' + t1mni + ' -ref ${FSLDIR}/data/standard/MNI152_T1_1mm -omat ' + t1mnimat + ' -dof 6')
+    os.system('/home/ajoshi/BrainSuite18a/bin/bse -i ' + t1 + ' -o ' + t1bse + ' --auto --trim -p')
+
+    os.system('flirt -in ' + t1bse + ' -nosearch -out ' + t1mni + ' -ref ${FSLDIR}/data/standard/MNI152_T1_1mm -omat ' + t1mnimat + ' -dof 6')
+
+    t1 = os.path.join(subdir, 'T1r.nii.gz')
+    t2mni = os.path.join(subdir, 'T1mni.nii.gz')
+    if os.path.isfile(t1):
+        # Apply the same transform (T1->MNI) to registered T2 to take it to mni space
+        os.system('flirt -in ' + t1 + ' -ref ' + t1mni +
+                  ' -applyxfm -init ' + t1mnimat)
 
     t2 = os.path.join(subdir, 'T2r.nii.gz')
     t2mni = os.path.join(subdir, 'T2mni.nii.gz')
     if os.path.isfile(t2):
         # Apply the same transform (T1->MNI) to registered T2 to take it to mni space
         os.system('flirt -in ' + t2 + ' -ref ' + t1mni +
-                  ' -dof 6 -cost normmi -init ' + t1mnimat + ' -out ' + t2mni)
+                  ' -applyxfm -init ' + t1mnimat)
 
     flair = os.path.join(subdir, 'FLAIRr.nii.gz')
     flairmni = os.path.join(subdir, 'FLAIRmni.nii.gz')
     if os.path.isfile(flair):
         # Apply the same transform (T1->MNI) to registered FLAIR to take it to mni space
         os.system('flirt -in ' + flair + ' -ref ' + t1mni +
-                  ' -dof 6 -cost normmi -init ' + t1mnimat + ' -out ' +
-                  flairmni)
+                  ' -applyxfm -init ' + t1mnimat)
 
 
 def main():
