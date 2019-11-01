06ed9aef8a7690048e769c34708f496c4af79cee
Anand A Joshi <ajoshi@erdos.usc.edu>
2019-11-01 10:55:05 -0700

    update

--- src/stats/main_TBM_stats.py
+++ src/stats/main_TBM_stats.py
@@ -13,12 +13,10 @@ def check_imgs_exist(studydir, sub_ids):
     subids_imgs = list()
 
     for id in sub_ids:
-        fname_T1 = os.path.join(studydir, id, 'T1mni.nii.gz')
-        fname_T2 = os.path.join(studydir, id, 'T2mni.nii.gz')
-        fname_FLAIR = os.path.join(studydir, id, 'FLAIRmni.nii.gz')
+        fname = os.path.join(studydir, id, 'BrainSuite',
+                             'T1mni.svreg.inv.jacobian.nii.gz')
 
-        if os.path.isfile(fname_T1) and os.path.isfile(
-                fname_T2) and os.path.isfile(fname_FLAIR):
+        if os.path.isfile(fname):
 
             subids_imgs.append(id)
 
@@ -37,22 +35,17 @@ def readsubs(studydir, sub_ids, nsub=10000):
         if n >= nsub:
             break
 
-        fname_T1 = os.path.join(studydir, id, 'T1mni.nii.gz')
-        fname_T2 = os.path.join(studydir, id, 'T2mni.nii.gz')
-        fname_FLAIR = os.path.join(studydir, id, 'FLAIRmni.nii.gz')
+        fname = os.path.join(studydir, id, 'BrainSuite',
+                             'T1mni.svreg.inv.jacobian.nii.gz')
         print('sub:', n, 'Reading', id)
-        t1 = ni.load_img(fname_T1)
-        t2 = ni.load_img(fname_T2)
-        flair = ni.load_img(fname_FLAIR)
+        jac = ni.load_img(fname)
 
         if n == 0:
-            data = np.zeros((3, min(len(sub_ids), nsub)) + t1.shape)
+            data = np.zeros((min(len(sub_ids), nsub), ) + jac.shape)
 
-            data[0, n, :, :, :] = t1.get_data()
-            data[1, n, :, :, :] = t2.get_data()
-            data[2, n, :, :, :] = flair.get_data()
+            data[n, :, :, :] = jac.get_data()
 
-    return data, sub_ids, flair
+    return data, sub_ids
 
 
 def main():
@@ -71,28 +64,27 @@ def main():
     epiIds = list(map(lambda x: x.strip(), epiIds))
     nonepiIds = list(map(lambda x: x.strip(), nonepiIds))
 
-    epi_data, epi_subids, t1 = readsubs(studydir, epiIds, nsub=20)
+    epi_data, epi_subids = readsubs(studydir, epiIds, nsub=20)
 
-    nonepi_data, nonepi_subids, _ = readsubs(studydir, nonepiIds, nsub=20)
+    nonepi_data, nonepi_subids = readsubs(studydir, nonepiIds, nsub=20)
 
-    epi_data = epi_data.reshape(epi_data.shape[0], epi_data.shape[1], -1)
-    nonepi_data = nonepi_data.reshape(nonepi_data.shape[0],
-                                      nonepi_data.shape[1], -1)
+    epi_data = epi_data.reshape(epi_data.shape[0], -1)
+    nonepi_data = nonepi_data.reshape(nonepi_data.shape[0], -1)
 
-    msk = t1.get_data().flatten() > 20
+    msk = nonepi_data[0, :].flatten() > 0
 
     numV = msk.sum()
 
     rval = sp.zeros(numV)
     pval = sp.zeros(numV)
 
-    edat1 = epi_data[0, :, msk].squeeze()
-    edat2 = nonepi_data[0, :, msk].squeeze()
+    edat1 = epi_data[:, msk].squeeze().T
+    edat2 = nonepi_data[:, msk].squeeze().T
 
     for nv in tqdm(range(numV)):
         rval[nv], pval[nv] = sp.stats.ranksums(edat1[nv, :], edat2[nv, :])
 
-    pval, t2 = hotelling_t2(nonepi_data[:, :, msk], nonepi_data[:, :, msk])
+    np.savez('TBM_results.npz', rval=rval, pval=pval)
 
     print('done')
 
--- src/stats/multivariate/hotelling.py
+++ src/stats/multivariate/hotelling.py
@@ -1,25 +1,36 @@
 import numpy as np
 from scipy.stats import f as f_distrib
 
-def hotelling_t2(X,Y):
 
-    nx=X.shape[1];ny=Y.shape[1];p=X.shape[0];
-    Xbar=X.mean(1);Ybar=Y.mean(1);
-    Xbar=Xbar.reshape(Xbar.shape[0],1,Xbar.shape[1])
-    Ybar=Ybar.reshape(Ybar.shape[0],1,Ybar.shape[1])
+def hotelling_t2(X, Y):
 
-    X_Xbar=X-Xbar
-    Y_Ybar=Y-Ybar
-    Wx=np.einsum('ijk,ljk->ilk',X_Xbar,X_Xbar)
-    Wy=np.einsum('ijk,ljk->ilk',Y_Ybar,Y_Ybar)
-    W=(Wx+Wy)/float(nx+ny-2)
-    Xbar_minus_Ybar=Xbar-Ybar
-    x = np.linalg.solve(W.transpose(2,0,1), Xbar_minus_Ybar.transpose(2,0,1))
-    x=x.transpose(1,2,0);
+    # X and Y are 3D arrays
+    # dim 0: number of features
+    # dim 1: number of subjects
+    # dim 2: number of mesh nodes or voxels
 
-    t2=np.sum(Xbar_minus_Ybar*x,0)
-    t2=t2*float(nx*ny)/float(nx+ny);
-    stat=(t2*float(nx+ny-1-p)/(float(nx+ny-2)*p));
 
-    pval=1-np.squeeze(f_distrib.cdf(stat,p,nx+ny-1-p));
-    return pval,t2
+    nx = X.shape[1]
+    ny = Y.shape[1]
+    p = X.shape[0]
+    Xbar = X.mean(1)
+    Ybar = Y.mean(1)
+    Xbar = Xbar.reshape(Xbar.shape[0], 1, Xbar.shape[1])
+    Ybar = Ybar.reshape(Ybar.shape[0], 1, Ybar.shape[1])
+
+    X_Xbar = X - Xbar
+    Y_Ybar = Y - Ybar
+    Wx = np.einsum('ijk,ljk->ilk', X_Xbar, X_Xbar)
+    Wy = np.einsum('ijk,ljk->ilk', Y_Ybar, Y_Ybar)
+    W = (Wx + Wy) / float(nx + ny - 2)
+    Xbar_minus_Ybar = Xbar - Ybar
+    x = np.linalg.solve(W.transpose(2, 0, 1),
+                        Xbar_minus_Ybar.transpose(2, 0, 1))
+    x = x.transpose(1, 2, 0)
+
+    t2 = np.sum(Xbar_minus_Ybar * x, 0)
+    t2 = t2 * float(nx * ny) / float(nx + ny)
+    stat = (t2 * float(nx + ny - 1 - p) / (float(nx + ny - 2) * p))
+
+    pval = 1 - np.squeeze(f_distrib.cdf(stat, p, nx + ny - 1 - p))
+    return pval, t2
