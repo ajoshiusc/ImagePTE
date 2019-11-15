83b7fb8596c6ea49dd76f3e9b05baa79efa13ccc
Anand A Joshi <ajoshi@erdos.usc.edu>
2019-11-14 17:11:42 -0800

    update

--- src/stats/main_lesion_stats.py
+++ src/stats/main_lesion_stats.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from multiprocessing import Pool
 import numpy as np
 from shutil import copyfile, copy
@@ -12,20 +13,42 @@ from statsmodels.stats.weightstats import ttest_ind
 #from statsmodels.stats import wilcoxon
 
 NSUB = 37 * 2
+ATLAS = '/home/ajoshi/BrainSuite19a/svreg/BCI-DNI_brain_atlas/BCI-DNI_brain.bfc.nii.gz'
+
+
+def warpsubs(studydir, lesion_studydir, sub_ids, nonepi):
+
+    print(len(sub_ids))
+
+    check_imgs_exist(studydir, sub_ids)
+
+    nsub = 37
+
+    print('Reading Subjects')
+
+    for n, id in enumerate(sub_ids):
+
+        invmap = os.path.join(studydir, id, 'BrainSuite',
+                              'T1mni.svreg.inv.map.nii.gz')
+
+        # Warp Lesion Map
+        fname_lesion = os.path.join(
+            lesion_studydir, id, 'MSE_T1_' + str(n + 37 * nonepi) + '.nii.gz')
+
+        fname_lesion_w = os.path.join(studydir, id, 'lesion_vae.atlas.nii.gz')
+        os.system('/home/ajoshi/BrainSuite19a/svreg/bin/svreg_apply_map.sh ' +
+                  invmap + ' ' + fname_lesion + ' ' + fname_lesion_w + ' ' + ATLAS)
 
 
 def check_imgs_exist(studydir, sub_ids):
-    subids_imgs = list()
 
     for id in sub_ids:
         fname = os.path.join(studydir, id, 'BrainSuite',
                              'T1mni.svreg.inv.jacobian.nii.gz')
 
-        if os.path.isfile(fname):
-
-            subids_imgs.append(id)
-
-    return subids_imgs
+        if not os.path.isfile(fname):
+            err_msg = 'the inv map does not exist for: ' + id, ' in dir: ' + studydir
+            sys.exit(err_msg)
 
 
 def readsubs(studydir):
@@ -49,6 +72,7 @@ def readsubs(studydir):
 def main():
 
     studydir = '/big_disk/akrami/git_repos_new/ImagePTE/src/Lesion Detection/models/3D_out'
+    studydir_imgs = '/big_disk/ajoshi/fitbir/preproc/maryland_rao_v1'
 
     epi_txt = '/big_disk/ajoshi/fitbir/preproc/maryland_rao_v1_epilepsy_imgs.txt'
     nonepi_txt = '/big_disk/ajoshi/fitbir/preproc/maryland_rao_v1_nonepilepsy_imgs.txt'
@@ -65,11 +89,22 @@ def main():
     epiIds = list(map(lambda x: x.strip(), epiIds))
     nonepiIds = list(map(lambda x: x.strip(), nonepiIds))
 
+    # Warp maps to match the atlas
+    warpsubs(studydir=studydir_imgs,
+             lesion_studydir=studydir,
+             sub_ids=epiIds,
+             nonepi=0)
+
+    warpsubs(studydir=studydir_imgs,
+             lesion_studydir=studydir,
+             sub_ids=nonepiIds,
+             nonepi=1)
+
     data = readsubs(studydir)
 
     epi_data = data[:int(NSUB / 2), :, :, :]
     nonepi_data = data[int(NSUB / 2):, :, :, :]
-#    readsubs(studydir, nonepiIds, NSUB=36)
+    #    readsubs(studydir, nonepiIds, NSUB=36)
 
     # Save mean over the epilepsy subjects
     epi_data_mean = ni.new_img_like(ati, epi_data.mean(axis=0))
@@ -103,7 +138,7 @@ def main():
     nonepi_data = nonepi_data.reshape(nonepi_data.shape[0], -1)
 
     msk = ati.get_data().flatten() > 0
-
+    #******warp to atlas*********
     numV = msk.sum()
 
     rval = sp.zeros(numV)
