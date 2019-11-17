c173eb1971a8ff9d92e2ef95a0d7def13cc20e33
Anand A Joshi <ajoshi@erdos.usc.edu>
2019-11-16 20:08:05 -0800

    update

--- src/stats/main_lesion_stats.py
+++ src/stats/main_lesion_stats.py
@@ -13,12 +13,14 @@ import scipy.stats as ss
 
 #from statsmodels.stats import wilcoxon
 
+sm = '.smooth3mm'
+
 
 def check_imgs_exist(studydir, sub_ids):
     subids_imgs = list()
 
     for id in sub_ids:
-        fname = os.path.join(studydir, id, 'lesion_vae.atlas.nii.gz')
+        fname = os.path.join(studydir, id, 'lesion_vae.atlas' + sm + '.nii.gz')
 
         if not os.path.isfile(fname):
             err_msg = 'the file does not exist: ' + fname
@@ -38,14 +40,14 @@ def readsubs(studydir, sub_ids):
 
     for n, id in enumerate(sub_ids):
 
-        fname = os.path.join(studydir, id, 'lesion_vae.atlas.nii.gz')
+        fname = os.path.join(studydir, id, 'lesion_vae.atlas' + sm + '.nii.gz')
         print('sub:', n, 'Reading', id)
-        jac = ni.load_img(fname)
+        im = ni.load_img(fname)
 
         if n == 0:
-            data = np.zeros((min(len(sub_ids), nsub), ) + jac.shape)
+            data = np.zeros((min(len(sub_ids), nsub), ) + im.shape)
 
-        data[n, :, :, :] = jac.get_data()
+        data[n, :, :, :] = im.get_data()
 
     return data, sub_ids
 
@@ -75,40 +77,41 @@ def main():
 
     # Save mean over the epilepsy subjects
     epi = ni.new_img_like(ati, epi_data.mean(axis=0))
-    epi.to_filename('epi_mean_lesion.nii.gz')
+    epi.to_filename('epi_mean_lesion' + sm + '.nii.gz')
 
     # Save std-dev over the epilepsy subjects
     epi = ni.new_img_like(ati, epi_data.std(axis=0))
-    epi.to_filename('epi_std_lesion.nii.gz')
+    epi.to_filename('epi_std_lesion' + sm + '.nii.gz')
 
     # Save mean over the non epilepsy subjects
     nonepi = ni.new_img_like(ati, nonepi_data.mean(axis=0))
-    nonepi.to_filename('nonepi_mean_lesion.nii.gz')
+    nonepi.to_filename('nonepi_mean_lesion' + sm + '.nii.gz')
 
     # Save std-dev over the non epilepsy subjects
     nonepi = ni.new_img_like(ati, nonepi_data.std(axis=0))
-    nonepi.to_filename('nonepi_std_lesion.nii.gz')
+    nonepi.to_filename('nonepi_std_lesion' + sm + '.nii.gz')
 
     # Save diff of mean over the non epilepsy subjects
     nonepi = ni.new_img_like(ati,
                              epi_data.mean(axis=0) - nonepi_data.mean(axis=0))
-    nonepi.to_filename('diffepi_mean_lesion.nii.gz')
+    nonepi.to_filename('diffepi_mean_lesion' + sm + '.nii.gz')
 
     # Save diff of std-dev over the non epilepsy subjects
     nonepi = ni.new_img_like(ati,
                              epi_data.std(axis=0) - nonepi_data.std(axis=0))
-    nonepi.to_filename('diffepi_std_lesion.nii.gz')
+    nonepi.to_filename('diffepi_std_lesion' + sm + '.nii.gz')
 
     epi_data = epi_data.reshape(epi_data.shape[0], -1)
     nonepi_data = nonepi_data.reshape(nonepi_data.shape[0], -1)
 
     msk = ati.get_data().flatten() > 0
+    pval_vol = np.ones(ati.shape)
+    rval_vol = np.ones(ati.shape)
 
     numV = msk.sum()
 
-    rval = sp.zeros(numV)
-    pval = sp.ones(numV)
-    
+    #   rval_vol = sp.zeros(numV)
+    #   pval_vol = sp.ones(numV)
 
     edat1 = epi_data[:, msk].squeeze().T
     edat2 = nonepi_data[:, msk].squeeze().T
@@ -124,34 +127,57 @@ def main():
     pval_vol = pval_vol.reshape(ati.shape)
 
     p = ni.new_img_like(ati, pval_vol)
-    p.to_filename('pval_lesion.nii.gz')
+    p.to_filename('pval_lesion' + sm + '.nii.gz')
 
     pval_vol = 0 * pval_vol.flatten()
     pval_vol[msk] = (pval < 0.05)
     pval_vol = pval_vol.reshape(ati.shape)
 
     p = ni.new_img_like(ati, pval_vol)
-    p.to_filename('pval_lesion.sig.mask.nii.gz')
+    p.to_filename('pval_lesion.sig.mask' + sm + '.nii.gz')
 
-    # Significance masks
+    # FDR corrected p values
+    _, pval_fdr = fdrcorrection(pvals=pval)
+
+    pval_vol = pval_vol.flatten()
+    pval_vol[msk] = pval_fdr
+    pval_vol = pval_vol.reshape(ati.shape)
+
+    p = ni.new_img_like(ati, pval_vol)
+    p.to_filename('pval_fdr_lesion' + sm + '.nii.gz')
+
+    pval_vol = 0 * pval_vol.flatten()
+    pval_vol[msk] = (pval_fdr < 0.05)
+    pval_vol = pval_vol.reshape(ati.shape)
+
+    p = ni.new_img_like(ati, pval_vol)
+    p.to_filename('pval_fdr_lesion.sig.mask' + sm + '.nii.gz')
+
+
+ '''   # Significance masks
     p1 = ni.smooth_img(p, 5)
-    p1.to_filename('pval_lesion_sig_mask.smooth5.nii.gz')
+    p1.to_filename('pval_lesion_sig_mask.smooth5' + sm + '.nii.gz')
 
     p1 = ni.smooth_img(p, 10)
-    p1.to_filename('pval_lesion_sig_mask.smooth10.nii.gz')
+    p1.to_filename('pval_lesion_sig_mask.smooth10' + sm + '.nii.gz')
 
     p1 = ni.smooth_img(p, 15)
-    p1.to_filename('pval_lesion_sig_mask.smooth15.nii.gz')
+    p1.to_filename('pval_lesion_sig_mask.smooth15' + sm + '.nii.gz')
+'''
 
     # Do f test
     F = epi_data.var(axis=0) / (nonepi_data.var(axis=0) + 1e-16)
 
     fimg = ni.new_img_like(ati, F.reshape(ati.shape))
-    fimg.to_filename('fval_lesion.nii.gz')
+    fimg.to_filename('fval_lesion' + sm + '.nii.gz')
 
-    single_tailed_pval = ss.f.cdf(F, 37-1, 37-1)
-    fimg = ni.new_img_like(ati, single_tailed_pval)
-    fimg.to_filename('pval_ftest_lesion.nii.gz')
+    pval = 1 - ss.f.cdf(F, 37 - 1, 37 - 1)
+    fimg = ni.new_img_like(ati, pval.reshape(ati.shape))
+    fimg.to_filename('pval_ftest_lesion' + sm + '.nii.gz')
+
+    _, pval_fdr = fdrcorrection(pvals=pval)
+    fimg = ni.new_img_like(ati, pval_fdr.reshape(ati.shape))
+    fimg.to_filename('pval_fdr_ftest_lesion' + sm + '.nii.gz')
 
 
     print('done')
