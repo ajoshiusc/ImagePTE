133af9f7c48e434e7d656934dc3c38028b3f99eb
Anand A Joshi <ajoshi@erdos.usc.edu>
2020-05-11 01:11:27 -0700

    update

--- src/preproc/main_copy4fitbir_upload.py
+++ src/preproc/main_copy4fitbir_upload.py
@@ -1,5 +1,5 @@
 import os
-from shutil import copyfile, make_archive
+from shutil import copyfile, make_archive, rmtree
 
 studydir = '/ImagePTE1/ajoshi/fitbir/preproc/maryland_rao_v1'
 
@@ -20,6 +20,7 @@ def main():
     nonepi_subids = list(map(lambda x: x.strip(), nonepiIds))
 
     out_dir = '/ImagePTE1/ajoshi/fitbir_upload_anatomical_analysis'
+    rmtree(out_dir)
     os.mkdir(out_dir)
 
     allids = nonepi_subids + epi_subids
@@ -28,14 +29,22 @@ def main():
 
         sub_dir = os.path.join(out_dir, id)
         os.mkdir(sub_dir)
+        '''
         lesion_mask = os.path.join(studydir, id, 'vae_mse.flair.mask.nii.gz')
         copyfile(lesion_mask, os.path.join(out_dir, id, 'lesion.mask.nii.gz'))
+        '''
 
         lesion_error = os.path.join(studydir, id, 'vae_mse.flair.nii.gz')
         copyfile(lesion_error, os.path.join(out_dir, id, 'lesion.nii.gz'))
 
         t1 = os.path.join(studydir, id, 'T1mni.nii.gz')
         copyfile(t1, os.path.join(out_dir, id, 'T1mni.nii.gz'))
+
+        t2 = os.path.join(studydir, id, 'T2mni.nii.gz')
+        copyfile(t2, os.path.join(out_dir, id, 'T2mni.nii.gz'))
+
+        flair = os.path.join(studydir, id, 'FLAIRmni.nii.gz')
+        copyfile(t2, os.path.join(out_dir, id, 'FLAIRmni.nii.gz'))
 
         l = os.path.join(studydir, id, 'BrainSuite',
                          'T1mni.left.mid.cortex.svreg.dfs')
