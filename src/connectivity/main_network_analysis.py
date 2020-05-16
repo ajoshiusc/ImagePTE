9b22f5df24e646333dc2814adbcd939f0f0350d9
Anand A Joshi <ajoshi@erdos.usc.edu>
2020-05-16 02:25:59 -0700

    update

--- src/connectivity/main_network_analysis.py
+++ src/connectivity/main_network_analysis.py
@@ -3,8 +3,10 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 
-c = np.load('hcp_graphs.npz')
-c = c['conn_mat']
+f = np.load('hcp_graphs.npz')
+c = f['conn_mat']
+lab_ids = f['label_ids']
+gordlab = f['labels']
 
 c = np.median(c, axis=2)
 np.fill_diagonal(c,0)
@@ -12,6 +14,15 @@ np.fill_diagonal(c,0)
 
 G = nx.convert_matrix.from_numpy_array(np.abs(c))
 cent = nx.eigenvector_centrality(G,weight='weight')
+
+cent=np.array(list(cent.items()))
+plt.hist(cent[:,1])
+plt.show()
+
+gord_cent = np.zeros(len(gordlab))
+
+for i,id in enumerate(lab_ids):
+    gord_cent[gordlab == id]=cent[i,1]
 
 
 print(nx.info(G))
