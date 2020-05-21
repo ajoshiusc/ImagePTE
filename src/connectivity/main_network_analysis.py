76effc4cca439933aea511db988ea207c573bd39
Anand A Joshi <ajoshi@masal.usc.edu>
2020-05-20 19:40:23 -0700

    added code for saving correlation matrices

--- src/connectivity/main_network_analysis.py
+++ src/connectivity/main_network_analysis.py
@@ -4,45 +4,53 @@ import sys
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
-
+from matplotlib.image import imsave
 from grayord_utils import visdata_grayord
 
 
-f = np.load('hcp_graphs.npz')
-c = f['conn_mat']
+f = np.load('fcon1000_graphs.npz')
+co = f['conn_mat']
 lab_ids = f['label_ids']
 gordlab = f['labels']
 
-c = np.mean(c, axis=2)
-np.fill_diagonal(c,0)
+c = np.mean(co, axis=2)
+np.fill_diagonal(c, 0)
 
 
 G = nx.convert_matrix.from_numpy_array(np.abs(c))
-cent = nx.eigenvector_centrality(G,weight='weight')
+cent = nx.eigenvector_centrality(G, weight='weight')
 
-cent=np.array(list(cent.items()))
-plt.hist(cent[:,1])
+cent = np.array(list(cent.items()))
+plt.hist(cent[:, 1])
 plt.show()
 
 gord_cent = np.zeros(len(gordlab))
 
-for i,id in enumerate(lab_ids):
-    gord_cent[gordlab == id] = cent[i,1]
+for i, id in enumerate(lab_ids):
+    gord_cent[gordlab == id] = cent[i, 1]
 
 
 visdata_grayord(data=gord_cent,
                 smooth_iter=100,
-                colorbar_lim=[0,.12],
+                colorbar_lim=[0, .12],
                 colormap='jet',
                 save_png=True,
-                surf_name='centrality_hcp',
+                surf_name='centrality_fcon1000',
                 out_dir='.',
                 bfp_path='/ImagePTE1/ajoshi/code_farm/bfp',
                 fsl_path='/usr/share/fsl')
 
+
+imsave('corr_ave_fcon1000.jpg', np.mean(co, axis=2),
+       vmin=-1.0, vmax=1.0, cmap='jet')
+
+imsave('corr_std_fcon1000.jpg', np.std(co, axis=2),
+       vmin=-1.0, vmax=1.0, cmap='jet')
+
+
 print(nx.info(G))
 
-pos = nx.spring_layout(G, weight='weight',iterations=1000)
+pos = nx.spring_layout(G, weight='weight', iterations=1000)
 plt.subplot(211)
 nx.draw(G, pos, with_labels=False, node_size=10)
 
