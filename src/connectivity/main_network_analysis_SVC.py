27ece77f3abac30a6f572bc36595d118ec7e403d
Anand A Joshi <ajoshi@masal.usc.edu>
2020-06-19 16:18:52 -0700

    This is the version that works well

---
+++ src/connectivity/main_network_analysis_SVC.py
@@ -0,0 +1,75 @@
+
+import sys
+
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import scipy.stats as ss
+from matplotlib.image import imsave
+from scipy.stats import norm
+from sklearn.metrics import auc, plot_roc_curve, roc_auc_score, roc_curve
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from statsmodels.stats.multitest import fdrcorrection
+from tqdm import tqdm
+from sklearn.ensemble import RandomForestClassifier
+from grayord_utils import visdata_grayord
+
+
+population = 'PTE'
+f = np.load(population+'_graphs.npz')
+conn_pte = f['conn_mat']
+lab_ids = f['label_ids']
+gordlab = f['labels']
+
+population = 'NONPTE'
+f = np.load(population+'_graphs.npz')
+conn_nonpte = f['conn_mat']
+lab_ids = f['label_ids']
+gordlab = f['labels']
+
+n_rois = conn_pte.shape[0]
+ind = np.tril_indices(n_rois, k=1)
+
+
+# Do SVM Analysis
+epi_measures = conn_pte[ind[0], ind[1], :].T
+nonepi_measures = conn_nonpte[ind[0], ind[1], :].T
+
+X = np.vstack((epi_measures, nonepi_measures))
+y = np.hstack(
+    (np.ones(epi_measures.shape[0]), np.zeros(nonepi_measures.shape[0])))
+
+n_iter = 20
+auc = np.zeros(n_iter)
+auc_t = np.zeros(n_iter)
+n_features = 25
+for t in range(n_iter):
+    X_train, X_test, y_train, y_test = train_test_split(X,
+                                                        y,
+                                                        test_size=0.33)
+    clf = RandomForestClassifier()
+
+    #clf = SVC(kernel='linear', C=1, gamma=0.0001, tol=1e-6)
+    clf.fit(X_train, y_train)
+    ind_feat = np.argsort(-clf.feature_importances_)
+
+    X_train, X_test, y_train, y_test = train_test_split(X,
+                                                        y,
+                                                        test_size=0.33)    
+    clf.fit(X_train[:, ind_feat[:n_features]], y_train)
+
+    #svc_disp = plot_roc_curve(clf, X_test, y_test)
+    y_score = clf.predict(X_test[:, ind_feat[:n_features]])
+    auc[t] = roc_auc_score(y_test, y_score)
+    y_score = clf.predict(X_train[:, ind_feat[:n_features]])
+    auc_t[t] = roc_auc_score(y_train, y_score)
+    print(auc[t], auc_t[t])
+
+print('running done')
+
+
+print(np.mean(auc), np.std(auc))
+print(np.mean(auc_t), np.std(auc_t))
+
+print('done')
