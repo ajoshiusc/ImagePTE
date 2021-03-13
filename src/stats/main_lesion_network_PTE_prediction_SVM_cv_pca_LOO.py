b830ab90a1d33a8d4af6e7b297af791f2c510b1e
Anand A Joshi <ajoshi@masal.usc.edu>
2021-03-13 10:12:41 -0800

    added LOO

--- src/stats/main_lesion_network_PTE_prediction_SVM_cv_pca_LOO.py
+++ src/stats/main_lesion_network_PTE_prediction_SVM_cv_pca_LOO.py
@@ -6,6 +6,7 @@ from sklearn.decomposition import PCA
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import StratifiedKFold, LeaveOneOut
 from sklearn.metrics import roc_auc_score
+from tqdm import tqdm
 
 f = np.load('../connectivity/PTE_graphs.npz')
 conn_pte = f['conn_mat']
@@ -58,23 +59,6 @@ best_c = 0
 max_AUC = 0
 C_range = [0.0001, 0.001, 0.01, .1, .3, .6, .9,
            1, 1.5, 2, 3, 4, 5, 6, 7, 9, 10, 100]
-for current_c in C_range:
-    clf = SVC(kernel='linear', C=current_c, tol=1e-9)
-    my_metric = 'roc_auc'
-    
-    cv = StratifiedKFold(n_splits=36, shuffle=True, random_state=1211)
-
-    auc = cross_val_score(clf, X, y, cv=cv, scoring=my_metric)
-    
-    #print(auc)
-
-    #auc = cross_val_score(clf, X, y, cv=cv, scoring=my_metric)
-    #print('AUC on testing data:gamma=%g, auc=%g' % (current_c, np.mean(auc)))
-    if np.mean(auc) >= max_AUC:
-        best_c = current_c
-        max_AUC = np.mean(auc)
-
-
 '''
 for mygamma in ['auto', 'scale']:
 clf = SVC(kernel='rbf', gamma=mygamma, tol=1e-9)
@@ -87,66 +71,59 @@ print('AUC on testing data:gamma=%s, auc=%g' % (mygamma, np.mean(auc)))
 # Following part of the code do a grid search to find best number of PCA component
 # the metric for comparing the performance is AUC
 ####################################################
-best_com = 0
+best_com = 53
 max_AUC = 0
-max_component = min((X.shape[0]-1), X.shape[1])
-for nf in range(1, max_component):
-    pipe = Pipeline([('pca_apply', PCA(n_components=nf, whiten=True)),
-                     ('svc', SVC(kernel='linear', C=best_c, tol=1e-9))])
-    kfold = StratifiedKFold(n_splits=36, shuffle=True, random_state=1211)
-    auc = cross_val_score(pipe, X, y, cv=kfold, scoring=my_metric)
-
-    # print('AUC after CV for nf=%dgamma=%s is %g' %
-    # (nf, best_c, np.mean(auc)))
-    if np.mean(auc) >= max_AUC:
-        best_com = nf
-        max_AUC = np.mean(auc)
 #######################selecting gamma################
 # Random permutation of pairs of training subject for 1000 iterations
 ####################################################
-iteration_num = 100
+iteration_num = 1
 auc_sum = np.zeros((iteration_num))
-for i in range(iteration_num):
-    # y = np.random.permutation(y)
+
+# y = np.random.permutation(y)
+#kfold = StratifiedKFold(n_splits=36, shuffle=True)
+#auc = cross_val_score(pipe, X, y, cv=kfold, scoring=my_metric)
+cv = LeaveOneOut()  # StratifiedKFold(n_splits=36, shuffle=True)
+
+y_pred = np.zeros(y.shape)
+for train, test in tqdm(cv.split(X, y)):
+
+    max_AUC = 0
+    for current_c in C_range:
+        clf = Pipeline([('svc', SVC(kernel='linear', C=current_c, tol=1e-9))])
+        my_metric = 'roc_auc'
+
+        cv = StratifiedKFold(n_splits=35, shuffle=True, random_state=1211)
+        auc = cross_val_score(
+            clf, X[train], y[train], cv=cv, scoring=my_metric)
+        if np.mean(auc) >= max_AUC:
+            best_c = current_c
+            max_AUC = np.mean(auc)
+
+    max_AUC = 0
+
+    max_component = min((X.shape[0]-4), X.shape[1])
+
+    for nf in range(5, max_component):
+        pipe = Pipeline([('pca_apply', PCA(n_components=nf, whiten=True)),
+                         ('svc', SVC(kernel='linear', C=best_c, tol=1e-9))])
+        kfold = StratifiedKFold(n_splits=35, shuffle=True, random_state=1211)
+        auc = cross_val_score(
+            pipe, X[train], y[train], cv=kfold, scoring=my_metric)
+
+        if np.mean(auc) >= max_AUC:
+            best_com = nf
+            max_AUC = np.mean(auc)
+
     clf = Pipeline([('pca_apply', PCA(n_components=best_com, whiten=True)),
-                     ('svc', SVC(kernel='linear', C=best_c, tol=1e-9))])
-    cv = LeaveOneOut() #StratifiedKFold(n_splits=36, shuffle=True)
+                    ('svc', SVC(kernel='linear', C=best_c, tol=1e-9))])
 
-    y_pred = np.zeros(y.shape)
-    for train, test  in cv.split(X, y):
-        clf.fit(X[train],y[train])
-        ytest = clf.predict(X[test])
-        y_pred[test] = ytest
-        #print(y[test],ytest)
+    clf.fit(X[train], y[train])
+    ytest = clf.predict(X[test])
+    y_pred[test] = ytest
 
-    auc = roc_auc_score(y, y_pred)
- 
-#    auc = cross_val_score(pipe, X, y, cv=kfold, scoring=my_metric)
-    auc_sum[i] = np.mean(auc)
-    # print('AUC after CV for i=%dgamma=%s is %g' %
-    # (i, best_gamma, np.mean(auc)))
+auc = roc_auc_score(y, y_pred)
+auc_sum = np.mean(auc)
 
 
-print('Average AUC with pca=%g , Std AUC=%g' % (np.mean(auc_sum), np.std(auc_sum)))
-
-for i in range(iteration_num):
-    # y = np.random.permutation(y)
-    clf = SVC(kernel='linear', C=best_c, tol=1e-9)
-    #kfold = StratifiedKFold(n_splits=36, shuffle=True)
-    #auc = cross_val_score(pipe, X, y, cv=kfold, scoring=my_metric)
-    cv = LeaveOneOut() #StratifiedKFold(n_splits=36, shuffle=True)
-
-    y_pred = np.zeros(y.shape)
-    for train, test  in cv.split(X, y):
-        clf.fit(X[train],y[train])
-        ytest = clf.predict(X[test])
-        y_pred[test] = ytest
-        #print(y[test],ytest)
-    auc = roc_auc_score(y, y_pred)
-
-    auc_sum[i] = np.mean(auc)
-    # print('AUC after CV for i=%dgamma=%s is %g' %
-    # (i, best_gamma, np.mean(auc)))
-
-
-print('Average AUC=%g , Std AUC=%g' % (np.mean(auc_sum), np.std(auc_sum)))
+print('Average AUC with PCA=%g , Std AUC=%g' %
+      (np.mean(auc_sum), np.std(auc_sum)))
