a99338861901613ba16d37568bda3a4b5163c120
Anand A. Joshi <ajoshi@usc.edu>
2022-07-06 21:33:18 -0700

    update

---
+++ src/brainsync_analysis/main_make_grpatlas_from_nonpte_training_bord.py
@@ -0,0 +1,43 @@
+import nilearn.image as ni
+import os
+from bfp_utils import load_bfp_data
+from brainsync import groupBrainSync, normalizeData
+import numpy as np
+import time
+
+
+studydir = '/ImagePTE1/ajoshi/fitbir/preproc/maryland_rao_v1'
+nonpte_training_txt = '/ImagePTE1/ajoshi/fitbir/preproc/maryland_rao_v1_nonepilepsy_imgs_training.txt'
+
+
+with open(nonpte_training_txt) as f:
+    subids = f.readlines()
+
+subids = list(map(lambda x: x.strip(), subids))
+
+sub_files = list()
+for sub in subids:
+    fname = os.path.join(studydir, sub, 'BFP', sub, 'func',
+                         sub + '_rest_bold.32k.GOrd.mat')
+
+    if os.path.isfile(fname):
+        sub_files.append(fname)
+    else:
+        print('File does not exist: %s' % fname)
+
+
+nonepi_data = load_bfp_data(sub_files, 171)
+
+t = time.time()
+X2, Os, Costdif, TotalError = groupBrainSync(nonepi_data)
+
+elapsed = time.time() - t
+
+np.savez('grp_atlas_unnormalized.npz', X2=X2, Os=Os)
+
+atlas_data, _, _ = normalizeData(X2)
+
+np.savez('grp_atlas.npz', atlas_data=atlas_data)
+
+
+# print(subids)
