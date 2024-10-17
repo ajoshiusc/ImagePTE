8af1deaf838cb97a5f4bc69ed6149cd48dda5cb5
Anand A. Joshi <ajoshi@usc.edu>
2024-10-16 19:45:24 -0700

    epibios analysis starting code

---
+++ epibiosx/main_lesion_analysis.py
@@ -0,0 +1,44 @@
+## Lesion analysis for EPIBIOSX data
+
+# Importing libraries
+import os
+import numpy as np
+import pandas as pd
+import nibabel as nib
+import glob
+from tqdm import tqdm
+
+epibiosx_path = '/deneb_disk/ifs/loni/faculty/dduncan/rgarner/shared/epibios'
+
+lesion_seg_path = os.path.join(epibiosx_path, 'human/data/lesion_segmentations_box/total_lesion_masks/second_review')
+
+
+# Load the lesion segmentation data and calculate the average map of the lesion
+
+lesion_seg_files = glob.glob(os.path.join(lesion_seg_path, '*.nii.gz'))
+
+lesion_seg_data = []
+for file in tqdm(lesion_seg_files):
+    v = nib.load(os.path.join(lesion_seg_path, file)).get_fdata()
+    lesion_seg_data.append(v)
+    print(v.shape)
+
+lesion_seg_data = np.array(lesion_seg_data)
+lesion_seg_data = np.mean(lesion_seg_data, axis=0)
+
+# Save the average lesion map
+lesion_seg_avg = nib.Nifti1Image(lesion_seg_data, affine=None)
+nib.save(lesion_seg_avg, os.path.join(lesion_seg_path, 'average_lesion_seg.nii.gz'))
+
+# Display the average lesion map
+import nilearn.plotting as nip
+
+nip.plot_roi(lesion_seg_avg, title='Average Lesion Segmentation', display_mode='z', cut_coords=5, draw_cross=False, cmap='hot', alpha=0.5)
+
+# save to png file
+nip.plot_roi(lesion_seg_avg, title='Average Lesion Segmentation', display_mode='z', cut_coords=5, draw_cross=False, cmap='hot', alpha=0.5, output_file='average_lesion_seg.png')
+
+
+
+
+
---
+++ epibiosx/main_preproc_mnireg.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import glob
+import os
+import shutil
+from fitbirpre import zip2nii, reg2mni_re, name2modality
+from multiprocessing import Pool
+from itertools import product, repeat
+import numpy as np
+import numbers
+from shutil import copyfile
+import time
+
+
+def regparfun(subid):
+
+    print(subid)
+    if not isinstance(subid, str):
+        return
+
+#    study_dir = '/big_disk/ajoshi/fitbir/tracktbi/MM_Prospective_ImagingMR_314'
+    # List of subjects that maryland_rao
+    preproc_dir = '/big_disk/ajoshi/fitbir/preproc'
+    study_name = 'tracktbi_pilot'
+
+    subdir = os.path.join(preproc_dir, study_name, subid)
+
+    t1 = os.path.join(subdir, 'T1r.nii.gz')
+    t1mnir = os.path.join(subdir, 'T1mni.nii.gz')
+    t1mnimask = os.path.join(subdir, 'T1mni.mask.nii.gz')
+    #    t1bfc = os.path.join(subdir, 'T1.bfc.nii.gz')
+
+    t1mnimat = os.path.join(subdir, 'T1mni.mat')
+    print(subid)
+
+    if not os.path.isfile(t1):
+        print('T1 does not exist for ' + subid + t1)
+        return
+
+    # register T1 image to MNI space
+
+    os.system('flirt -in ' + t1 + ' -out ' + t1mnir +
+              ' -ref ${FSLDIR}/data/standard/MNI152_T1_1mm_brain.nii.gz -omat '
+              + t1mnimat + ' -dof 6 -cost normmi')
+
+    t1 = os.path.join(subdir, 'T1r.nii.gz')
+    t1mni = os.path.join(subdir, 'T1mni.nii.gz')
+
+    if os.path.isfile(t1):
+        # Create mask
+        os.system('fslmaths ' + t1mni + ' -bin ' + t1mnimask)
+        # Apply the same transform (T1->MNI)
+
+        t2 = os.path.join(subdir, 'T2r.nii.gz')
+        t2mni = os.path.join(subdir, 'T2mni.nii.gz')
+    if os.path.isfile(t2):
+        # Apply the same transform (T1->MNI) to registered T2
+        os.system('flirt -in ' + t2 + ' -ref ' + t1mnir + ' -out ' + t2mni +
+                  ' -applyxfm -init ' + t1mnimat)
+        os.system('fslmaths ' + t2mni + ' -mul ' + t1mnimask + ' ' + t2mni)
+
+    flair = os.path.join(subdir, 'FLAIRr.nii.gz')
+    flairmni = os.path.join(subdir, 'FLAIRmni.nii.gz')
+    if os.path.isfile(flair):
+        # Apply the same transform (T1->MNI) to registered FLAIR
+        os.system('flirt -in ' + flair + ' -ref ' + t1mnir + ' -out ' +
+                  flairmni + ' -applyxfm -init ' + t1mnimat)
+        os.system('fslmaths ' + flairmni + ' -mul ' + t1mnimask + ' ' +
+                  flairmni)
+
+
+def main():
+    #Set subject dirs
+    med_hist_csv = '/big_disk/ajoshi/fitbir/tracktbi_pilot/Baseline Med History_246/TrackTBI_MedicalHx.csv'
+    subIds = pd.read_csv(med_hist_csv, index_col=1)
+    pool = Pool(processes=12)
+    tbi_done_list = '/big_disk/ajoshi/fitbir/preproc/tracktbi_pilot_done.txt'
+
+    with open(tbi_done_list) as f:
+        tbidoneIds = f.readlines()
+
+    tbidoneIds = list(map(lambda x: x.strip(), tbidoneIds))
+
+    print(subIds.index)
+    subsnotdone = [x for x in subIds.index if x not in tbidoneIds]
+    
+#    regparfun(subsnotdone[2])
+    
+    print('++++++++++++++')
+    pool.map(regparfun, subsnotdone)
+
+    pool.close()
+    pool.join()
+
+
+if __name__ == "__main__":
+    main()
