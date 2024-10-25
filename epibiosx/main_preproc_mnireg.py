11a376a3cb2bad315c251f2b9a133bd88855458d
Anand A. Joshi <ajoshi@usc.edu>
2024-10-25 00:44:48 -0700

    update

--- epibiosx/main_preproc_mnireg.py
+++ epibiosx/main_preproc_mnireg.py
@@ -19,15 +19,75 @@ def regparfun(subid):
 
 #    study_dir = '/big_disk/ajoshi/fitbir/tracktbi/MM_Prospective_ImagingMR_314'
     # List of subjects that maryland_rao
-    preproc_dir = '/big_disk/ajoshi/fitbir/preproc'
-    study_name = 'tracktbi_pilot'
+    preproc_dir = '/deneb_disk/epibiosx_data/preproc'
 
-    subdir = os.path.join(preproc_dir, study_name, subid)
+    subdir = os.path.join(preproc_dir, subid)
+    if not os.path.isdir(subdir):
+        os.makedirs(subdir)
 
-    t1 = os.path.join(subdir, 'T1r.nii.gz')
+    t1_img = glob.glob(os.path.join('/deneb_disk/ifs/loni/faculty/dduncan/rgarner/shared/epibios/human/data/t1mprage/', subid + '*t1mprage.nii.gz'))
+    
+    if len(t1_img) == 0:
+        print('T1 does not exist for ' + subid)
+        return
+    
+    if len(t1_img) > 1:
+        print('Multiple T1 images found for ' + subid)
+        return
+
+    t2_img = glob.glob(os.path.join('/deneb_disk/ifs/loni/faculty/dduncan/rgarner/shared/epibios/human/data/t2/', subid + '*t2.nii.gz'))
+    
+    if len(t2_img) == 0:
+        print('T2 does not exist for ' + subid)
+        return
+    
+    if len(t2_img) > 1:
+        print('Multiple T2 images found for ' + subid)
+        return
+
+
+    t1 = os.path.join(subdir, 'T1.nii.gz')
+    t2 = os.path.join(subdir, 'T2.nii.gz')
+    os.system('cp ' + t1_img[0] + ' ' + t1)
+    os.system('cp ' + t2_img[0] + ' ' + t2)
+
+    t1bse = os.path.join(subdir, 'T1.bse.nii.gz')
+    t1bfc = os.path.join(subdir, 'T1.bfc.nii.gz')
+
+    if not os.path.isfile(t1bse):
+        os.system('bet ' + t1 + ' ' + t1bse + ' -f .3 -A2 ' + t2)
+
+    # apply bias field correction using bfc from BrainSuite
+    if not os.path.isfile(t1bfc):
+        os.system('/home/ajoshi/Software/BrainSuite23a/bin/bfc -i ' + t1bse + ' -o ' + t1bfc)
+
+
+    # use the bias field corrected image for registration to mni space
     t1mnir = os.path.join(subdir, 'T1mni.nii.gz')
+    t1mnimat = os.path.join(subdir, 'T1mni.mat')
+    t1mnimask = os.path.join(subdir, 'T1mni.mask.nii.gz')
+
+
+    if not os.path.isfile(t1mnir):
+        os.system('flirt -in ' + t1bfc + ' -ref ${FSLDIR}/data/standard/MNI152_T1_1mm_brain.nii.gz -out ' + t1mnir + ' -omat ' + t1mnimat + ' -dof 6 -cost normmi')
+        os.system('fslmaths ' + t1mnir + ' -bin ' + t1mnimask)
+
+
+
+
+
+    
+    if not os.path.isfile(t1bfc):
+        os.system('fast -t 1 -n 3 -H 0.1 -I 4 -l 20.0 -o ' + t1bfc + ' ' + t1bse)
+
+    '''t1mnir = os.path.join(subdir, 'T1mni.nii.gz')
     t1mnimask = os.path.join(subdir, 'T1mni.mask.nii.gz')
     #    t1bfc = os.path.join(subdir, 'T1.bfc.nii.gz')
+
+    os.system('bet ' + t1_img + ' ' + t1bse + ' -f .3')
+    os.system('fast -t 1 -n 3 -H 0.1 -I 4 -l 20.0 -o ' + t1bfc + ' ' + t1bse)
+    os.system('flirt -in ' + t1bfc + ' -ref ${FSLDIR}/data/standard/MNI152_T1_1mm_brain.nii.gz -out ' + t1mnir + ' -omat ' + t1mnimat + ' -dof 6 -cost normmi')
+    os.system('fslmaths ' + t1mnir + ' -bin ' + t1mnimask)
 
     t1mnimat = os.path.join(subdir, 'T1mni.mat')
     print(subid)
@@ -42,7 +102,7 @@ def regparfun(subid):
               ' -ref ${FSLDIR}/data/standard/MNI152_T1_1mm_brain.nii.gz -omat '
               + t1mnimat + ' -dof 6 -cost normmi')
 
-    t1 = os.path.join(subdir, 'T1r.nii.gz')
+    
     t1mni = os.path.join(subdir, 'T1mni.nii.gz')
 
     if os.path.isfile(t1):
@@ -67,29 +127,43 @@ def regparfun(subid):
         os.system('fslmaths ' + flairmni + ' -mul ' + t1mnimask + ' ' +
                   flairmni)
 
+    '''
+
 
 def main():
-    #Set subject dirs
-    med_hist_csv = '/big_disk/ajoshi/fitbir/tracktbi_pilot/Baseline Med History_246/TrackTBI_MedicalHx.csv'
-    subIds = pd.read_csv(med_hist_csv, index_col=1)
+
+
+    pte_xlsx = os.path.join('spreadsheets/short PTE.xlsx')
+
+
+    # read the spreadsheet with the PTE data, and extract the subject IDs and PTE status
+    pte_data = pd.read_excel(pte_xlsx)
+    pte_data = pte_data[['Study ID', 'PTE', 'LS']]
+    pte_data = pte_data.dropna(subset=['Study ID'])
+
+    subIds = pte_data['Study ID'].values
+
     pool = Pool(processes=12)
-    tbi_done_list = '/big_disk/ajoshi/fitbir/preproc/tracktbi_pilot_done.txt'
+    #done_list = '/big_disk/ajoshi/fitbir/preproc/tracktbi_pilot_done.txt'
+    doneIds=[]
 
-    with open(tbi_done_list) as f:
-        tbidoneIds = f.readlines()
+    #with open(tbi_done_list) as f:
+    #    tbidoneIds = f.readlines()
 
-    tbidoneIds = list(map(lambda x: x.strip(), tbidoneIds))
+    #tbidoneIds = list(map(lambda x: x.strip(), tbidoneIds))
 
-    print(subIds.index)
-    subsnotdone = [x for x in subIds.index if x not in tbidoneIds]
+    print(subIds)
+    subsnotdone = [x for x in subIds if x not in doneIds]
     
-#    regparfun(subsnotdone[2])
+    for sub in subsnotdone:
+        regparfun(sub)
     
-    print('++++++++++++++')
+    '''print('++++++++++++++')
     pool.map(regparfun, subsnotdone)
 
     pool.close()
     pool.join()
+    '''
 
 
 if __name__ == "__main__":
